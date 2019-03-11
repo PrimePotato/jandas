9d76860242a254689698a7057643f161b469d18b
Robert Cooper <robertjohncooper@gmail.com>
2019-03-11 12:00:07 +0000

    Added simple performance tester

---
+++ src/test/java/io/github/primepotato/jandas/utils/PerformanceTesterTest.java
@@ -0,0 +1,34 @@
+package io.github.primepotato.jandas.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.*;
+
+public class PerformanceTesterTest {
+
+    PerformanceTester pt;
+
+    Consumer<Void> randGen = new Consumer<Void>() {
+        @Override
+        public void accept(Void aVoid) {
+            for (int i=0; i<1000; ++i){
+                Math.random();
+            }
+        }
+    };
+
+    @Before
+    public void setUp() throws Exception {
+
+        pt = new PerformanceTester(randGen,100000);
+    }
+
+    @Test
+    public void run() {
+        System.out.println(pt.run());
+    }
+}
