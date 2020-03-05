ca55443e61621522ede84afbc92b081fedc6348b
Rob <robertjohncooper@gmail.com>
2020-03-05 11:17:07 +0000

    bob

--- src/main/java/io/github/primepotato/jandas/io/parsers/impl/DateParser.java
+++ src/main/java/io/github/primepotato/jandas/io/parsers/impl/DateParser.java
@@ -13,85 +13,85 @@ import java.util.stream.Collectors;
 
 public class DateParser extends AbstractParser<LocalDate> {
 
-  private static final List<String> acceptedPatterns =
-      Arrays.asList("dd/MM/yyyy", "yyyyMMdd");
+    private static final List<String> acceptedPatterns =
+            Arrays.asList("dd/MM/yyyy", "yyyyMMdd");
 
-  private static DateTimeFormatter getDefaultFormatter() {
+    private static DateTimeFormatter getDefaultFormatter() {
 
-    DateTimeFormatterBuilder dfb = new DateTimeFormatterBuilder();
-    acceptedPatterns.forEach(p -> dfb.appendOptional(DateTimeFormatter.ofPattern(p)));
-    return dfb.toFormatter();
-  }
-
-  public static final DateTimeFormatter DEFAULT_FORMATTER = getDefaultFormatter();
-
-  private Locale locale = Locale.getDefault();
-  private DateTimeFormatter formatter = DEFAULT_FORMATTER;
-
-  public DateParser() {
-
-    this((Iterable<String>) null, null, null);
-  }
-
-  public DateParser(String missingValueStrings, DateTimeFormatter formatter, Locale locale) {
-
-    this(Arrays.stream(missingValueStrings.split(","))
-        .map(s -> s.trim())
-        .collect(Collectors.toList()), formatter, locale);
-  }
-
-  public DateParser(String[] missingValueStrings, DateTimeFormatter formatter, Locale locale) {
-
-    this(Arrays.asList(missingValueStrings), formatter, locale);
-  }
-
-  public DateParser(Iterable<String> missingValueStrings, DateTimeFormatter formatter,
-      Locale locale) {
-
-    super(missingValueStrings);
-    this.formatter = formatter != null ? formatter : this.formatter;
-    this.locale = locale != null ? locale : this.locale;
-  }
-
-  @Override
-  public boolean canParse(String s) {
-
-    if (isMissing(s)) {
-      return true;
+        DateTimeFormatterBuilder dfb = new DateTimeFormatterBuilder();
+        acceptedPatterns.forEach(p -> dfb.appendOptional(DateTimeFormatter.ofPattern(p)));
+        return dfb.toFormatter();
     }
 
-    try {
-      LocalDate.parse(s, formatter.withLocale(locale));
-      return true;
-    } catch (DateTimeParseException e) {
-      // it's all part of the plan
-      return false;
+    public static final DateTimeFormatter DEFAULT_FORMATTER = getDefaultFormatter();
+
+    private Locale locale = Locale.getDefault();
+    private DateTimeFormatter formatter = DEFAULT_FORMATTER;
+
+    public DateParser() {
+
+        this((Iterable<String>) null, null, null);
     }
-  }
 
-  public void setCustomFormatter(DateTimeFormatter f) {
+    public DateParser(String missingValueStrings, DateTimeFormatter formatter, Locale locale) {
 
-    formatter = f;
-  }
-
-  public void setLocale(Locale locale) {
-
-    this.locale = locale;
-  }
-
-  @Override
-  public LocalDate parse(String s) {
-
-    if (isMissing(s)) {
-      return null;
+        this(Arrays.stream(missingValueStrings.split(","))
+                .map(s -> s.trim())
+                .collect(Collectors.toList()), formatter, locale);
     }
-    return LocalDate.parse(s, formatter);
-  }
 
-  @Override
-  public Class<LocalDate> elementClass() {
+    public DateParser(String[] missingValueStrings, DateTimeFormatter formatter, Locale locale) {
 
-    return LocalDate.class;
-  }
+        this(Arrays.asList(missingValueStrings), formatter, locale);
+    }
+
+    public DateParser(Iterable<String> missingValueStrings, DateTimeFormatter formatter,
+                      Locale locale) {
+
+        super(missingValueStrings);
+        this.formatter = formatter != null ? formatter : this.formatter;
+        this.locale = locale != null ? locale : this.locale;
+    }
+
+    @Override
+    public boolean canParse(String s) {
+
+        if (isMissing(s)) {
+            return true;
+        }
+
+        try {
+            LocalDate.parse(s, formatter.withLocale(locale));
+            return true;
+        } catch (DateTimeParseException e) {
+            // it's all part of the plan
+            return false;
+        }
+    }
+
+    public void setCustomFormatter(DateTimeFormatter f) {
+
+        formatter = f;
+    }
+
+    public void setLocale(Locale locale) {
+
+        this.locale = locale;
+    }
+
+    @Override
+    public LocalDate parse(String s) {
+
+        if (isMissing(s)) {
+            return null;
+        }
+        return LocalDate.parse(s, formatter);
+    }
+
+    @Override
+    public Class<LocalDate> elementClass() {
+
+        return LocalDate.class;
+    }
 
 }
